14d9dc0702bc1341dd4b609bbbfd0f5f04476b7f
Josip Sito <josip.sito@objectbay.com>
2020-05-05 14:58:45 +0200

    Updates

---
+++ precache-manifest.583741438e13c40712151c734b6cbd5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b44288b910860982849460e09ea1a8",
+    "url": "/sitonja.github.io/index.html"
+  },
+  {
+    "revision": "11e187210f464d505c80",
+    "url": "/sitonja.github.io/static/css/main.7ec96f73.chunk.css"
+  },
+  {
+    "revision": "eafaff2371af64a0e30e",
+    "url": "/sitonja.github.io/static/js/2.0065b818.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sitonja.github.io/static/js/2.0065b818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e187210f464d505c80",
+    "url": "/sitonja.github.io/static/js/main.790253eb.chunk.js"
+  },
+  {
+    "revision": "ca4106260322ecd0026c",
+    "url": "/sitonja.github.io/static/js/runtime-main.79127576.js"
+  }
+]);
